06a1dad110c226513c1fb3483e9433cc59280a8e
t3knomanser@gmail.com <t3knomanser@gmail.com>
2013-06-19 19:23:23 -0400

    Dropdowns completed, with some object examples.

--- Code/04-Dropdowns/cupcake.js
+++ Code/04-Dropdowns/cupcake.js
@@ -18,7 +18,14 @@ var CupcakeVM = function()  {
 //Now our VM holds a list
 var GlobalVM = function() {
 	this.Flavors = ko.observableArray(["Chocolate", "Vanilla", "Red Velvet", "Peanut Butter", "Bacon", "Sriracha"]);
-	this.Decorations = ko.observableArray(["Sprinkles", "Chocolate Syrup", "Colored Sugar", "Bacon Bits", "Sriracha", "Plastic Mustache"]);
+	//Decorations will be a custom object
+	this.Decorations = ko.observableArray([
+		{Id: ko.observable(1), Name: ko.observable("Sprinkles")}, //since these don't change, they don't technically need to be observables
+		{Id: ko.observable(2), Name: ko.observable("Chocolate Syrup")}, 
+		{Id: ko.observable(3), Name: ko.observable("Colored Sugar")}, 
+		{Id: ko.observable(4), Name: ko.observable("Bacon Bits")}, 
+		{Id: ko.observable(5), Name: ko.observable("Sriracha")}, 
+			{Id: ko.observable(6), Name: ko.observable("Plastic Mustache")}]);
 	this.Cupcakes = ko.observableArray([]);
 	this.NewCake = new CupcakeVM();
 	var self = this;
