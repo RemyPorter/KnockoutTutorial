23e111ea4176d565e2aeaa461e24d62cc81dd0a7
Remy Porter <t3knomanser@gmail.com>
2013-06-18 21:28:48 -0400

    Fixed the observable arrays. Started adding the slides.

--- Code/03-ObservableArrays/cupcake.js
+++ Code/03-ObservableArrays/cupcake.js
@@ -2,14 +2,26 @@ var CupcakeVM = function()  {
 	this.CakeFlavor = ko.observable("");
 	this.IcingFlavor = ko.observable("");
 	this.Decoration = ko.observable("");
+	this.Clear = function() {
+		this.CakeFlavor("");
+		this.IcingFlavor("");
+		this.Decoration("");
+	}
+	this.Copy = function() {
+		var res = new CupcakeVM();
+		res.CakeFlavor(this.CakeFlavor());
+		res.IcingFlavor(this.IcingFlavor());
+		res.Decoration(this.Decoration());
+		return res;
+	}
 }
 //Now our VM holds a list
 var GlobalVM = function() {
 	this.Cupcakes = ko.observableArray([]);
 	this.NewCake = new CupcakeVM();
 	this.AddCake = function() {
-		this.Cupcakes.push(this.NewCake);
-		this.NewCake = new CupcakeVM();
+		this.Cupcakes.push(this.NewCake.Copy());
+		this.NewCake.Clear();
 	}
 }
 var vm = new GlobalVM();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-KnockoutTutorial
+Knockout Tutorial
 ================
+This is a collection of slides and code samples which will guide the user through building a simple, client-only application. It is a linear guide.
