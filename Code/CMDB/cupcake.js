053856496970775af9d23537c8d7def88b26dc45
t3knomanser@gmail.com <t3knomanser@gmail.com>
2013-06-29 08:01:56 -0400

    ?

--- Code/CMDB/cupcake.js
+++ Code/CMDB/cupcake.js
@@ -26,7 +26,7 @@ var GlobalVM = function() {
 		{Id: ko.observable(3), Name: ko.observable("Colored Sugar")}, 
 		{Id: ko.observable(4), Name: ko.observable("Bacon Bits")}, 
 		{Id: ko.observable(5), Name: ko.observable("Sriracha")}, 
-			{Id: ko.observable(6), Name: ko.observable("Plastic Mustache")}]);
+		{Id: ko.observable(6), Name: ko.observable("Plastic Mustache")}]);
 	this.Cupcakes = ko.observableArray([]);
 	this.NewCake = ko.observable(new CupcakeVM());
 
