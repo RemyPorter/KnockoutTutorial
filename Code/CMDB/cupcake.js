45eca93ebe08d6ea23927986687ea241af80a4b0
t3knomanser@gmail.com <t3knomanser@gmail.com>
2013-06-30 13:53:43 -0400

    Started doing a little styling, and fixing up some of the code.

--- Code/CMDB/cupcake.js
+++ Code/CMDB/cupcake.js
@@ -23,18 +23,11 @@ var GlobalVM = function() {
 	//Decorations will be a custom object
 	this.Decorations = ko.observableArray([
 		{Id: ko.observable(1), Name: ko.observable("Sprinkles")}, //since these don't change, they don't technically need to be observables
-		{Id: ko.observable(2), Name: ko.observable("Chocolate Syrup")}, 
-		{Id: ko.observable(3), Name: ko.observable("Colored Sugar")}, 
-		{Id: ko.observable(4), Name: ko.observable("Bacon Bits")}, 
-		{Id: ko.observable(5), Name: ko.observable("Sriracha")}, 
-		{Id: ko.observable(6), Name: ko.observable("Plastic Mustache")}]);
+		{Id: ko.observable(2), Name: ko.observable("Colored Sugar")}, 
+		{Id: ko.observable(3), Name: ko.observable("Bacon Bits")}, 
+		{Id: ko.observable(4), Name: ko.observable("Plastic Mustache")}]);
 	this.Cupcakes = ko.observableArray([]);
 	this.NewCake = ko.observable(new CupcakeVM());
-
-	this.ImagifyIcing = function(flavor) {
-		if (! flavor) { return "..Images/transparent.gif"; }
-		return "../Images/icing." + flavor.toLowerCase().replace(' ', '') + '.png';
-	}
 
 	var self = this;
 	//ko.computed sees that this depends on observables
@@ -49,6 +42,10 @@ var GlobalVM = function() {
 	this.AddCake = function() {
 		self.Cupcakes.push(this.Copy());
 		this.Clear();
+	}
+
+	this.RemoveCake = function(cake) {
+		self.Cupcakes.splice(self.Cupcakes.indexOf(cake), 1);
 	}
 }
 var vm = new GlobalVM();
